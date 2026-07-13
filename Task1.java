d706069235e5deb3f63d85109c9b4ecd0e978d83
Maiyada Albarwani <mbarwani27@gmail.com>
2026-07-13 08:43:00 +0400

    Task 313

---
+++ Task1.java
@@ -0,0 +1,16 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+        Double length = 12.5;
+        Double width = 8.0;
+        Double area = length * width;
+        /*fromula to calculate area*/
+        Double perimeter = (2 * length + width) ;
+        /*Formula to calculate perimeter*/
+
+    System.out.println("Length: " + length);
+    System.out.println("Width: " + width);
+    System.out.println("Area: " + area);
+    System.out.println("perimeter: " + perimeter);
+    }
+}
